16b0d983d1d92b500afb1d2b065c4782e6676024
erebusx <plutox@gmail.com>
2022-04-29 13:57:02 +0800

    Update scrobble.js
    支持一次多首

--- module/scrobble.js
+++ module/scrobble.js
@@ -1,22 +1,31 @@
 // 听歌打卡
 
 module.exports = (query, request) => {
-  const data = {
-    logs: JSON.stringify([
-      {
-        action: 'play',
-        json: {
-          download: 0,
-          end: 'playend',
-          id: query.id,
-          sourceId: query.sourceid,
-          time: query.time,
-          type: 'song',
-          wifi: 0,
-          source: 'list',
-        },
+  id_a = query.id.split(',')
+  id_l = id_a.length
+  sid_a = query.sourceid.split(',')
+  sid_l = sid_a.length
+  t_a = query.time.split(',')
+  t_l = t_a.length
+  buf = []
+  
+  for (var i=0; i<id_l; i++) {
+    buf.push({
+      action: 'play',
+      json: {
+        download: 0,
+        end: 'playend',
+        id: id_a[i],
+        sourceId: sid_a[i%sid_l],
+        time: t_a[i%t_l],
+        type: 'song',
+        wifi: 0,
+        source: 'list',
       },
-    ]),
+    })
+
+  const data = {
+    logs: JSON.stringify(buf),
   }
 
   return request('POST', `https://music.163.com/weapi/feedback/weblog`, data, {
